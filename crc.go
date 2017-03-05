bdc10d003ff79ed2b10465c3a751a2ce2591ff6d
Vladimir Burlakov <vvb0x56@gmail.com>
2017-03-05 20:20:27 +0300

    crc8fast

--- crc.go
+++ crc.go
@@ -1,32 +1,96 @@
 package main
 
+// http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
+
 import "fmt"
 
 const (
-	polinom byte = 0xb5
+	polinom byte = 0x1d
 )
+
+var crc8table [256]byte
 
 // --- MAIN ---
 
 func main() {
-	var b byte = 0xd6
+	b := []byte{0x01, 0x02}
 
-	fmt.Printf("slow: %08b\n", crcSlow(b))
-	fmt.Printf("slow: %08b\n", crcNaive(b))
+	crc8init()
 
-	fmt.Printf("0xd6: %08b\n", 0xd6)
-	fmt.Printf("0xd6: %08b\n", 0xb5)
+	//printByteAsHex(crc8table)
+
+	//printByteAsHex(b)
+	fmt.Printf("crc: 0x%x\n", crc8(b))
+	fmt.Printf("crc fast 0x%x\n", crc8fast(b))
+	fmt.Printf("0x%x\n", crc8table[0x1f])
+}
+
+func crc8(b []byte) byte {
+
+	var remainder byte = 0x00
+
+	for y := 0; y < len(b); y++ {
+		remainder ^= b[y]
+
+		for i := 8; i != 0; i-- {
+			if remainder&0x80 != 0 {
+				remainder = (remainder << 1) ^ polinom
+			} else {
+				remainder = remainder << 1
+			}
+		}
+
+	}
+	return remainder
 
 }
+
+func crc8fast(b []byte) byte {
+	var crc byte = 0x00
+
+	for i := 0; i < len(b); i++ {
+		tmp := crc ^ b[i]
+		crc = crc8table[tmp]
+	}
+
+	return crc
+}
+
+func crc8init() {
+	//var y byte
+	var currentByte byte
+	for y := 0; y < len(crc8table); y++ {
+
+		currentByte = byte(y)
+
+		for i := 0; i < 8; i++ {
+			if currentByte&0x80 != 0 {
+				currentByte = (currentByte << 1) ^ polinom
+			} else {
+				currentByte = currentByte << 1
+			}
+		}
+		crc8table[y] = currentByte
+	}
+}
+
+func printByteAsHex(b [256]byte) {
+	for i := 0; i < len(b); i++ {
+		fmt.Printf("%d) 0x%x\n ", i, b[i])
+	}
+}
+
+/*
 
 func crcSlow(message byte) byte {
 	//var crc byte = 0xFF
 
-	for i := 0; i < 7; i++ {
-		if message&0x80 == 0x80 {
-			message ^= polinom
+	for i := 0; i < 8; i++ {
+		if message&0x80 != 0 {
+			message = message ^ polinom
 		}
 		message = (message << 1)
+
 	}
 
 	return message
@@ -45,7 +109,7 @@ func crcNaive(message byte) byte {
 	return remainder
 }
 
-/*
+
 func crcSlow(message []byte) byte {
 	var remainder byte = 0xFF
 
